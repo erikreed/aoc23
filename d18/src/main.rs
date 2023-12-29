ae2ca1b903542f5bbba4f75bae92e3197c6c84c2
Erik Reed <ereed.uw@gmail.com>
2023-12-29 12:34:40 -0800

    d19

--- d18/src/main.rs
+++ d18/src/main.rs
@@ -13,6 +13,20 @@ type Path = Vec<Coord>;
 
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let path = parse_path()?;
+
+    let line = LineString::from_iter(path.iter().map(|&(y, x)| (y as f64, x as f64)));
+    let polygon = Polygon::new(
+        line,
+        vec![],
+    );
+    let area = polygon.unsigned_area() + polygon.exterior().euclidean_length() / 2.0 + 1.0;
+    println!("area: {}", area.round() as u64);
+
+    Ok(())
+}
+
+fn parse_path() -> Result<Vec<(i32, i32)>, Box<dyn Error>> {
     let reader = BufReader::new(File::open("d18.txt")?);
 
     let mut path = vec![(0, 0)];
@@ -20,7 +34,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         let line = line?;
         let parts = line.split_whitespace().collect::<Vec<_>>();
 
-        let hex_distance = i32::from_str_radix(&parts[2][2..7], 16).unwrap();
+        let hex_distance = i32::from_str_radix(&parts[2][2..7], 16)?;
         let hex_dir = parts[2].as_bytes()[parts[2].len() - 2] - '0' as u8;
 
         let (dir, distance);
@@ -50,15 +64,5 @@ fn main() -> Result<(), Box<dyn Error>> {
         path.push((previous.0 + delta.0, previous.1 + delta.1));
     }
     assert_eq!(path[0], path[path.len() - 1]);
-
-
-    let line = LineString::from_iter(path.iter().map(|&(y, x)| (y as f64, x as f64)));
-    let polygon = Polygon::new(
-        line,
-        vec![],
-    );
-    let area = polygon.unsigned_area() + polygon.exterior().euclidean_length() / 2.0 + 1.0;
-    println!("area: {}", area);
-
-    Ok(())
+    Ok(path)
 }
